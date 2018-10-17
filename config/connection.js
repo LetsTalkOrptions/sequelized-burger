3accddcf8c4edeedf7d7d79981ac85da924f4611
jcress <cresswellj@outlook.com>
2018-10-16 22:46:48 -0400

    finished code, having problems with app.post is not a function error..

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,13 @@
-// *********************************************************************************
-// CONNECTION.JS - THIS FILE INITIATES THE CONNECTION TO MYSQL
-// *********************************************************************************
+var Sequelize = require('sequelize'), connection;
 
-// Dependencies
-var Sequelize = require("sequelize");
+if (process.env.JAWSDB_URL) {
+  connection = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  connection = new Sequelize('burgers_db', 'root', 'root', {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: '3000'
+  })
+};
 
-// Creates mySQL connection using Sequelize, the empty string in the third argument spot is our password.
-var sequelize = new Sequelize("burgers_db", "root", "root", {
-  host: "localhost",
-  port: 3306,
-  dialect: "mysql",
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000
-  }
-});
-
-// Exports the connection for other files to use
-module.exports = sequelize;
+module.exports = connection;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require("express");
 
 var app = express();
+var bodyParser = require('body-parser');
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
@@ -15,12 +16,14 @@ app.engine("handlebars", exphbs({
 }));
 app.set("view engine", "handlebars");
 
-var routes = require("./controllers/burgers_controller");
+var routes = require("./controllers/burgers_controller.js");
 
-app.use(routes);
+app.use('/', routes);
 
 // listen on port 3000
 var PORT = process.env.PORT || 3000;
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
+
+module.exports = app;
